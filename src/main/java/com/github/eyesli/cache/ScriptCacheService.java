56ada7a68115d8bb8d40965073aba74919ff96f7
Levi Li <453321481@qq.com>
2023-12-19 02:07:07 +0800

    init

---
+++ src/main/java/com/github/eyesli/cache/ScriptCacheService.java
@@ -0,0 +1,19 @@
+package com.github.eyesli.cache;
+
+
+import groovy.lang.Binding;
+import groovy.lang.Script;
+
+import java.io.IOException;
+
+/**
+ * @author Levi Li
+ * @since 09/18/2023
+ */
+public interface ScriptCacheService {
+
+
+    Script parseScript(String scriptText, Binding binding) throws IOException;
+
+
+}
