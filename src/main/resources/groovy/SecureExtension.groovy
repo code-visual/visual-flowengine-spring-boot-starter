73938de58faeb7fd718d010e55341becc8dc3972
Levi Li <453321481@qq.com>
2024-02-26 22:23:43 +0800

    底层调用逻辑

--- src/main/resources/groovy/SecureExtension.groovy
+++ src/main/resources/groovy/SecureExtension.groovy
@@ -1,14 +1,22 @@
 package groovy
 
+import org.codehaus.groovy.ast.expr.MethodCallExpression
+
 onMethodSelection { expr, methodNode ->
-    if (expr.methodAsString == "decision_rule") {
-        return
-    }
-    if (methodNode.declaringClass.name=='java.lang.System'
-            ||methodNode.declaringClass.name=='java.lang.Runtime'
-            ||methodNode.declaringClass.name=='java.lang.Class') {
-        addStaticTypeError("Method  is not allowed!", expr)
+
+//    GroovyTypeCheckingExtensionSupport groovyTypeCheckingExtensionSupport = new GroovyTypeCheckingExtensionSupport();
+
+    if (expr instanceof MethodCallExpression) {
+        // 检查方法名
+        if (expr.methodAsString == 'decision_rule') {
+            return // 如果是decision_rule方法，就不进行后续的安全检查
+        }
     }
 
-
+    // 安全检查，防止调用敏感方法
+    if (methodNode.declaringClass.name=='java.lang.System' ||
+            methodNode.declaringClass.name=='java.lang.Runtime' ||
+            methodNode.declaringClass.name=='java.lang.Class') {
+        addStaticTypeError("Method is not allowed!", expr)
+    }
 }
