6bedec5f3a746cf0d1cc63ff01825f9a986500b0
Levi Li <Levi.Li@tdsynnex.com>
2024-02-26 20:08:16 +0800

    静态编译需要跳过动态的DSL

--- src/main/resources/groovy/SecureExtension.groovy
+++ src/main/resources/groovy/SecureExtension.groovy
@@ -1,6 +1,9 @@
 package groovy
 
 onMethodSelection { expr, methodNode ->
+    if (expr.methodAsString == "decision_rule") {
+        return
+    }
     if (methodNode.declaringClass.name=='java.lang.System'
             ||methodNode.declaringClass.name=='java.lang.Runtime'
             ||methodNode.declaringClass.name=='java.lang.Class') {
